10fd1bf4eddb87e3b75f646f82edd7f273db28d2
Enoch Phetteplace <curiousaps@gmail.com>
2020-05-16 20:59:38 -0500

    App works right now.

--- app/src/main/java/com/curiousapps/dagger2practice/di/AppComponent.java
+++ app/src/main/java/com/curiousapps/dagger2practice/di/AppComponent.java
@@ -11,6 +11,7 @@ import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+@Singleton
 @Component(
         modules = {
                 AndroidSupportInjectionModule.class,
--- app/src/main/java/com/curiousapps/dagger2practice/di/AppModule.java
+++ app/src/main/java/com/curiousapps/dagger2practice/di/AppModule.java
@@ -18,21 +18,24 @@ import dagger.Provides;
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     static RequestOptions provideRequestOptions(){
         return RequestOptions
                 .placeholderOf(R.drawable.white_background)
-                .error(R.drawable.white_background);
+                .error(R.drawable.ic_launcher_foreground);
     }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions){
         return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions);
     }
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable(Application application){
-        return ContextCompat.getDrawable(application, R.drawable.logo);
+        return ContextCompat.getDrawable(application, R.drawable.cca_logo);
     }
 }
